b8980e0b19bd5fa366f06befd8ea40447ab7ce06
sadneo <haywardeel@gmail.com>
2026-09-23 09:30:43 -0400

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "My default dev flake.";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+          ];
+        };
+      });
+    };
+}
